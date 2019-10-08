4aea60d6e2ac648e15df69c7fcb9792f5238e377
git <2389311433@qq.com>
2019-10-08 15:44:55 +0800

    优化调整

--- eureka-consumer/src/main/java/com/zy/eureka/consumer/EurekaConsumerBootStrap.java
+++ eureka-consumer/src/main/java/com/zy/eureka/consumer/EurekaConsumerBootStrap.java
@@ -2,14 +2,14 @@ package com.zy.eureka.consumer;
 
 import com.zy.eureka.consumer.client.EurekServerProvider;
 import feign.Logger;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @AUTHOR zhangy
@@ -22,6 +22,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableCircuitBreaker
 @EnableAspectJAutoProxy
 //@EnableHystrixDashboard
+@EnableSwagger2
+@Aspect
 //@SpringBootApplication == @EnableAutoConfiguration + @ComponentScan
 //@ComponentScan("com.zy.eureka.consumer.controller")
 public class EurekaConsumerBootStrap {
